bd2e2ec08478dd73b103436143dc5e8ceda2befa
chen.tao <hilerchyn@gmail.com>
2026-04-14 14:14:32 +0800

    feat: 封装发送和接收过程为独立的函数

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,128 @@ mod args;
 use chrono::{Local, Utc};
 use clap::Parser;
 use influxdb3::InfluxDbClientBuilder;
+use influxdb3::http_client::InfluxDbClient;
 use influxdb3::{DataPointBuilder, FieldDataType};
-use serialport;
+use serialport::{self, SerialPort};
 use std::io::Read;
 use std::thread;
 use std::time::Duration;
+
+// 串口接收函数
+async fn rx_function<F>(
+    args: &args::Args,
+    serial_port: &mut Box<dyn SerialPort>,
+    influxdb_client: &InfluxDbClient,
+    callback: F,
+) where
+    F: Fn(&args::Args, &mut Box<dyn SerialPort>),
+{
+    if !args.rx_enable {
+        return;
+    }
+
+    if args.rx_sleep > 0 {
+        thread::sleep(Duration::from_millis(args.rx_sleep));
+    }
+
+    let mut serial_buf: Vec<u8> = vec![0; 256];
+    let now = Local::now();
+    let formatted_time_str = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    match serial_port.read(serial_buf.as_mut_slice()) {
+        Ok(t) => {
+            // 't' is the number of bytes read
+            if args.debug {
+                println!("[{formatted_time_str}] Received: {:?}", &serial_buf[..t]);
+            }
+
+            println!(
+                "[{formatted_time_str}] Received: {:?}",
+                String::from_utf8(serial_buf.clone())
+            );
+
+            if args.influx_enable {
+                // WRITING
+                let data_point = DataPointBuilder::new()
+                    .table("signle")
+                    .field("point", FieldDataType::Integer(99))
+                    .datetime(Utc::now())
+                    .build()
+                    .unwrap();
+
+                match influxdb_client.write_one(data_point).await {
+                    Ok(cluster_uuid_opt) => {
+                        println!(
+                            "[{formatted_time_str}] writing db successful : cluster_uuid = {:?}",
+                            cluster_uuid_opt
+                        );
+                    }
+                    Err(error_detail) => {
+                        println!(
+                            "[{formatted_time_str}] write db failure : {:?}",
+                            error_detail
+                        );
+                    }
+                }
+            }
+
+            callback(args, serial_port);
+        }
+        Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+        Err(e) => {
+            eprintln!("[{formatted_time_str}] {:?}", e);
+
+            if args.influx_enable {
+                // WRITING
+                let data_point = DataPointBuilder::new()
+                    .table("signle")
+                    .field("point", FieldDataType::Integer(199))
+                    .datetime(Utc::now())
+                    .build()
+                    .unwrap();
+
+                match influxdb_client.write_one(data_point).await {
+                    Ok(cluster_uuid_opt) => {
+                        println!(
+                            "[{formatted_time_str}] writing db successful : cluster_uuid = {:?}",
+                            cluster_uuid_opt
+                        );
+                    }
+                    Err(error_detail) => {
+                        println!(
+                            "[{formatted_time_str}] write db failure : {:?}",
+                            error_detail
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
+
+// 串口发送函数
+fn tx_function(args: &args::Args, serial_port: &mut Box<dyn SerialPort>) {
+    // 收到消息以后再写入消息
+    if !args.tx_enable {
+        return;
+    }
+
+    if args.tx_sleep > 0 {
+        thread::sleep(Duration::from_millis(args.tx_sleep));
+    }
+
+    let now = Local::now();
+    let formatted_time_str = now.format("%Y-%m-%d %H:%M:%S").to_string();
+    let buf = "from_mac_mini".as_bytes();
+    match serial_port.write(buf) {
+        Ok(_) => {
+            println!("[{formatted_time_str}] write success");
+        }
+        Err(e) => {
+            println!("[{formatted_time_str}] write error: {}", e);
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -38,101 +155,11 @@ async fn main() {
         .expect("Failed to open port");
 
     // 2. Create a buffer to hold the incoming data
-    let mut serial_buf: Vec<u8> = vec![0; 256];
+    // WARN: 全局缓存
+    //let mut serial_buf: Vec<u8> = vec![0; 256];
 
     // 3. Continuously read in a loop
     loop {
-        let now = Local::now();
-        let formatted_time_str = now.format("%Y-%m-%d %H:%M:%S").to_string();
-
-        if args.rx_enable {
-            match port.read(serial_buf.as_mut_slice()) {
-                Ok(t) => {
-                    // 't' is the number of bytes read
-                    if args.debug {
-                        println!("[{formatted_time_str}] Received: {:?}", &serial_buf[..t]);
-                    }
-
-                    println!(
-                        "[{formatted_time_str}] Received: {:?}",
-                        String::from_utf8(serial_buf.clone())
-                    );
-
-                    if args.influx_enable {
-                        // WRITING
-                        let data_point = DataPointBuilder::new()
-                            .table("signle")
-                            .field("point", FieldDataType::Integer(99))
-                            .datetime(Utc::now())
-                            .build()
-                            .unwrap();
-
-                        match influxdb_client.write_one(data_point).await {
-                            Ok(cluster_uuid_opt) => {
-                                println!(
-                                    "[{formatted_time_str}] writing db successful : cluster_uuid = {:?}",
-                                    cluster_uuid_opt
-                                );
-                            }
-                            Err(error_detail) => {
-                                println!(
-                                    "[{formatted_time_str}] write db failure : {:?}",
-                                    error_detail
-                                );
-                            }
-                        }
-                    }
-                }
-                Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
-                Err(e) => {
-                    eprintln!("[{formatted_time_str}] {:?}", e);
-
-                    if args.influx_enable {
-                        // WRITING
-                        let data_point = DataPointBuilder::new()
-                            .table("signle")
-                            .field("point", FieldDataType::Integer(199))
-                            .datetime(Utc::now())
-                            .build()
-                            .unwrap();
-
-                        match influxdb_client.write_one(data_point).await {
-                            Ok(cluster_uuid_opt) => {
-                                println!(
-                                    "[{formatted_time_str}] writing db successful : cluster_uuid = {:?}",
-                                    cluster_uuid_opt
-                                );
-                            }
-                            Err(error_detail) => {
-                                println!(
-                                    "[{formatted_time_str}] write db failure : {:?}",
-                                    error_detail
-                                );
-                            }
-                        }
-                    }
-                }
-            }
-
-            if args.rx_sleep > 0 {
-                thread::sleep(Duration::from_millis(args.rx_sleep));
-            }
-        }
-
-        if args.tx_enable {
-            if args.tx_sleep > 0 {
-                thread::sleep(Duration::from_millis(args.tx_sleep));
-            }
-
-            let buf = "from_mac_mini".as_bytes();
-            match port.write(buf) {
-                Ok(_) => {
-                    println!("[{formatted_time_str}] write success");
-                }
-                Err(e) => {
-                    println!("[{formatted_time_str}] write error: {}", e);
-                }
-            }
-        }
+        rx_function(&args, port.by_ref(), &influxdb_client, tx_function).await;
     }
 }
